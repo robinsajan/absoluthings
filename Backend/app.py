3419b725c4ae37effbe3342bf6bbbadf6a478525
Robin Sajan <robinsajan4#gmail.com>
2026-06-07 22:58:24 +0530

    deploy-ready

--- Backend/app.py
+++ Backend/app.py
@@ -278,4 +278,8 @@ def init_db():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=False, port=5000)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=False
+    )
